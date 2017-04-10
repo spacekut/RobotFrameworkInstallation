4d14857dd7934b472b74e43c42306481336b3383
spacekut <katcabison@ymail.com>
2017-04-10 11:14:26 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup (
+	name="SproutHR",
+	version="1.0",
+	description="Exam",
+	author="Kat Cabison",
+	author_email="katrinac@payrollpie.com",
+	install_requires=[
+		'robotframework',
+		'robotframework-selenium2library',
+		'chromedriver_installer'
+	]
+)
